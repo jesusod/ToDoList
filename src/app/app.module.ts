0fb7ecd1f105a13c06ea13277c8e3bcffa3c410d
jesusod <jesusoterodevesa@gmail.com>
2022-05-25 09:08:45 +0200

    all funcionalitys done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoItemComponent,
+    EditTodoDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTodoDialogComponent } from '../edit-todo-dialog/edit-todo-dialog.component';
 import { DataService } from '../shared/data.service';
 import { Todo } from '../shared/todo.model';
 
@@ -12,7 +14,7 @@ export class TodosComponent implements OnInit {
 
   todos: Todo[]
 
-  constructor( private dataService: DataService) { }
+  constructor( private dataService: DataService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
 
@@ -27,6 +29,34 @@ export class TodosComponent implements OnInit {
 
     form.reset()
 
+  }
+
+  toggleCompleted(todo: Todo){
+      todo.completed= !todo.completed
+  }
+
+  editTodo(todo: Todo){
+
+    const index = this.todos.indexOf(todo)
+
+    let dialogRef = this.dialog.open(EditTodoDialogComponent, {
+      width: '700px',
+      data: todo
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+
+      if (result){
+        this.dataService.updateTodo(index, result)
+      }
+
+    })
+  }
+
+  deleteTodo(todo: Todo){
+
+    const index = this.todos.indexOf(todo)
+    this.dataService.deleteTodo(index)
   }
 
 }
